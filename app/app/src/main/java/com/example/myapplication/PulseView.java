8aec997194ebc152bfe3945d1679c5cb81e37bab
zhijie <guszhiwe@student.gu.se>
2021-09-21 12:41:47 +0200

    add app

---
+++ app/app/src/main/java/com/example/myapplication/PulseView.java
@@ -0,0 +1,127 @@
+package com.example.myapplication;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+
+import androidx.annotation.Nullable;
+
+public class PulseView extends View {
+
+    // For painting the circles
+    private Paint paint;
+    private float maxRadius;
+    private float initialRadius;
+    private float pulseGap;
+    Resources res = getResources();
+    int color = res.getColor(R.color.turquoise);
+
+    // For animation interaction
+    private int concentration;
+
+    // For animation
+    private ValueAnimator pulseAnimator;
+    private float pulseOffset;
+    private int initialAlpha;
+    private int fade;
+    private long duration;
+
+    public PulseView(Context context) {
+        super(context);
+
+        init(null);
+    }
+
+    public PulseView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        init(attrs);
+    }
+
+    public PulseView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        init(attrs);
+    }
+
+    private void init(@Nullable AttributeSet set) {
+        // For painting the circles
+        this.paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        this.paint.setStrokeWidth(4);
+        this.paint.setStyle(Paint.Style.STROKE);
+        this.paint.setColor(color);
+        this.maxRadius = 0f;
+        this.initialRadius = 0f;
+        this.pulseGap = 100f;
+
+        // For animation interaction
+        this.concentration = 1;
+        int minFade = 40;
+        int maxFade = 100;
+        long maxDuration = 2000L;
+
+        // For animating the circles
+        this.pulseOffset = 0f;
+        this.duration = maxDuration / concentration;
+        this.initialAlpha = 255;
+        this.fade = minFade + ((maxFade - minFade) / concentration);
+    }
+
+    // Setter for changing the concentration level depending on EEG
+    public void setConcentration(int concentration) {
+        this.concentration = concentration;
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        pulseAnimator = ValueAnimator.ofFloat(0f, pulseGap);
+        pulseAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator updatedAnimation) {
+                pulseOffset = (float) updatedAnimation.getAnimatedValue();
+                postInvalidateOnAnimation();
+            }
+        });
+        pulseAnimator.setDuration(duration);
+        pulseAnimator.setRepeatMode(ValueAnimator.RESTART);
+        pulseAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        pulseAnimator.setInterpolator(new LinearInterpolator());
+
+        pulseAnimator.start();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        pulseAnimator.cancel();
+    }
+
+    @Override
+    protected void onDraw(final Canvas canvas) {
+        super.onDraw(canvas);
+
+        float centerX = getX() + getWidth() / 2f;
+        float centerY = getY() + getHeight() / 2f;
+
+        maxRadius = getWidth() / 3f * 2f;
+        float currentRadius = initialRadius + pulseOffset;
+
+        int currentAlpha = initialAlpha;
+
+        do {
+            paint.setAlpha(currentAlpha);
+            currentAlpha -= fade;
+
+            canvas.drawCircle(centerX, centerY, currentRadius, paint);
+            currentRadius += pulseGap;
+
+        } while (currentRadius < maxRadius);
+    }
+}
---
+++ arduino/smartcar/smartcar.ino
@@ -0,0 +1,233 @@
+#include <Smartcar.h>
+#include <BluetoothSerial.h>
+#include <SimpleTimer.h>
+
+
+bool cdObstacle = false; // Used for disabling the SR04 sensors for some time.
+
+int input;
+int currentSpeed; // The speed the car is set to move at, used for manual drive only
+const int TRIGGER_PIN = 5; //   D5 red cable
+const int ECHO_PIN = 18; // D18 green cable
+const int TRIGGER_PIN2 = 17;// D17 white cable
+const int ECHO_PIN2 = 16; //    D16 blue cable
+const unsigned int MAX_DISTANCE = 100;
+SR04 sensor(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+SR04 backsensor(TRIGGER_PIN2, ECHO_PIN2, MAX_DISTANCE);
+const int GYROSCOPE_OFFSET = 37;
+const unsigned long PRINT_INTERVAL = 100;
+unsigned long previousPrintout = 0;
+const auto pulsesPerMeter = 600;
+const int fSpeed    = 50;  // 50% of the full speed forward
+const int bSpeed    = -50; // 50% of the full speed backward
+const int tankSpeed = 55;  // Speed at which the wheels turn on the spot
+const int flDegrees = -152; // degrees to turn forward left
+const int frDegrees = 152;  // degrees to turn forward right
+const int blDegrees = -152; // degrees to turn backward left
+const int brDegrees = 152; // degrees to turn backward right
+SimpleTimer coolDown(5000); //
+BluetoothSerial bluetooth;
+BrushedMotor leftMotor(smartcarlib::pins::v2::leftMotorPins);
+BrushedMotor rightMotor(smartcarlib::pins::v2::rightMotorPins);
+DifferentialControl control(leftMotor, rightMotor);
+GY50 gyroscope(GYROSCOPE_OFFSET);
+DirectionlessOdometer leftOdometer(
+    smartcarlib::pins::v2::leftOdometerPin, []() { leftOdometer.update(); }, pulsesPerMeter);
+DirectionlessOdometer rightOdometer(
+    smartcarlib::pins::v2::rightOdometerPin, []() { rightOdometer.update(); }, pulsesPerMeter);
+SmartCar car(control, gyroscope, leftOdometer, rightOdometer);
+
+void setup()
+{
+    Serial.begin(9600);
+    bluetooth.begin("Car"); // Device name
+    currentSpeed = 0;
+}
+
+void loop()
+{
+    handleInput();
+   // Serial.println(input);
+   // Serial.println(sensor.getDistance());
+   // Serial.println(backsensor.getDistance());
+   
+}
+
+void rotateOnSpot(int targetDegrees, int speed) // taken from smartcar library
+{
+    speed = smartcarlib::utils::getAbsolute(speed);
+    targetDegrees %= 360; // put it on a (-360,360) scale
+    if (!targetDegrees)
+        return; // if the target degrees is 0, don't bother doing anything
+    /* Let's set opposite speed on each side of the car, so it rotates on spot */
+    if (targetDegrees > 0)
+    { // positive value means we should rotate clockwise
+        car.overrideMotorSpeed(speed,
+                               -speed); // left motors spin forward, right motors spin backward
+    }
+    else
+    { // rotate counter clockwise
+        car.overrideMotorSpeed(-speed,
+                               speed); // left motors spin backward, right motors spin forward
+    }
+    const auto initialHeading = car.getHeading(); // the initial heading we'll use as offset to
+                                                  // calculate the absolute displacement
+    int degreesTurnedSoFar
+        = 0; // this variable will hold the absolute displacement from the beginning of the rotation
+    while (abs(degreesTurnedSoFar) < abs(targetDegrees))
+    { // while absolute displacement hasn't reached the (absolute) target, keep turning
+        car.update(); // update to integrate the latest heading sensor readings
+        auto currentHeading = car.getHeading(); // in the scale of 0 to 360
+        if ((targetDegrees < 0) && (currentHeading > initialHeading))
+        { // if we are turning left and the current heading is larger than the
+            // initial one (e.g. started at 10 degrees and now we are at 350), we need to substract
+            // 360, so to eventually get a signed
+            currentHeading -= 360; // displacement from the initial heading (-20)
+        }
+        else if ((targetDegrees > 0) && (currentHeading < initialHeading))
+        { // if we are turning right and the heading is smaller than the
+            // initial one (e.g. started at 350 degrees and now we are at 20), so to get a signed
+            // displacement (+30)
+            currentHeading += 360;
+        }
+        degreesTurnedSoFar
+            = initialHeading - currentHeading; // degrees turned so far is initial heading minus
+                                               // current (initial heading
+        // is at least 0 and at most 360. To handle the "edge" cases we substracted or added 360 to
+        // currentHeading)
+    }
+    changeSpeed(0); // we have reached the target, so stop the car
+}
+
+void handleInput()
+{ 
+    while (bluetooth.available()){ input = bluetooth.read(); }        
+    car.update();
+    int front = sensor.getDistance(); 
+    int back = backsensor.getDistance();
+    
+    
+    while(front != 0 && front < 20 && !cdObstacle) { 
+        car.getHeading();
+        changeSpeed(0);
+        car.setAngle(0);
+        delay(100);
+        rotateOnSpot(-180, 80);
+        front = sensor.getDistance();
+        cdObstacle = true;
+        coolDown.reset();
+        
+    }
+     if (coolDown.isReady()) 
+        {                
+        cdObstacle = false;
+    }
+    
+    while(back != 0 && back < 20 && !cdObstacle){
+        car.getHeading();
+        changeSpeed(0);
+        car.setAngle(0);
+        delay(100);
+        rotateOnSpot(-180, 80);
+        back = backsensor.getDistance();
+        cdObstacle = true;
+        coolDown.reset();
+        back = backsensor.getDistance();
+    }
+    
+
+    // Converts inputs which were neagtive back to negative
+    if (input > 127){
+        input = (256 - input);
+        input = (input * -1);   
+    }
+
+    switch (input) {
+        //  manual  |   mindcontrol plus phone gyroscope
+        //  8 1 2   |   9
+        //  7   3   |   10
+        //  6 5 4   |   -110 to -11 and 11 to 110
+
+        case 1: // Move straight forward
+            car.setAngle(0);
+            changeSpeed(fSpeed);
+            break;
+        
+        case 2: // Turn right forward
+            car.setAngle(frDegrees);
+            changeSpeed(fSpeed);
+            break;
+
+        case 3: // turn clockwise 
+            car.setAngle(0);
+            rotateOnSpot(90, 80);
+            break;
+
+        case 4: // Turn right backward
+            car.setAngle(brDegrees);
+            changeSpeed(bSpeed);
+            break;
+
+        case 5: // Move straight backward
+            car.setAngle(0);
+            changeSpeed(bSpeed);
+            break;
+
+        case 6: // Turn left backward
+            car.setAngle(blDegrees);
+            changeSpeed(bSpeed);
+            break;
+
+        case 7: // turn counterclockwise
+            car.setAngle(0);
+            rotateOnSpot(-90, 80);
+            break;
+
+        case 8: // turn left forward
+            car.setAngle(flDegrees);
+            changeSpeed(fSpeed);
+            break;
+        case 13: //stop
+            car.setAngle(0);
+            changeSpeed(0);
+            break;
+
+        case 9: // Mindcontrol: move forwards
+            changeSpeed(fSpeed);
+            break;
+
+        case 10: // Mindcontrol: stop
+            changeSpeed(0);
+            break;
+
+        default: // Inputs are angles for mindcontrol steering
+            car.setAngle(-input);
+    }
+}
+
+// Motors can't switch direction without delay
+// When speed is set to something other than 0;
+void changeSpeed(int targetSpeed)
+{
+    if (currentSpeed == 0){
+        car.setSpeed(targetSpeed);
+        currentSpeed = targetSpeed;
+    } 
+    else if (currentSpeed < targetSpeed){ // backward to forward
+        car.setSpeed(0);
+        delay(100);
+        car.setSpeed(targetSpeed);
+        currentSpeed = targetSpeed;
+    }
+    else if (currentSpeed > targetSpeed){ // forward to backward
+        car.setSpeed(0);
+        delay(100);
+        car.setSpeed(targetSpeed);
+        currentSpeed = targetSpeed;
+    } else {} // targetSpeed == currentSpeed. Do nothing
+}
+
+
+
+
+
